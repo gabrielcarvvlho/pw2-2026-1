4172bb29d3c7ea54dfd5630f09b261a34a60f0e5
Luiz Chaves <lucachaves@gmail.com>
2026-03-04 17:36:06 -0300

    classroom: add examples for control flow statements and operators

---
+++ classroom/ecma/expression.js
@@ -0,0 +1,76 @@
+// error: unary, binary, ternary operator
+// console.log(20 - +++ 10 * 2);
+
+// precedence: MDN Operator precedence and associativity
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_precedence#table
+// - precedence 11
+// * precedence 12
+console.log(20 - 10 * 2);
+
+// associativity
+// - precedence 11, associativity right-to-left
+console.log(20 - 10 - 5);
+// *, / precedence 12, associativity left-to-right
+console.log(20 * 10 / 5);
+
+// return value
+1 + 1; // 2
+
+// coercion
+console.log('1' + 1); // '11', string concatenation
+console.log('1' - 1); // 0, numeric subtraction, '1' coerced to 1
+console.log(+'1'); // 1, unary plus, '1' coerced to 1
+console.log(Number('1')); // 1, Number constructor, '1' coerced to 1
+
+// equality operator
+console.log(1 == 1); // true
+console.log(1 === 1); // true
+console.log(1 == '1'); // true, type coercion
+console.log(1 === '1'); // false, strict equality, no type coercion
+
+// bitwise operators
+// 1 in binary: 0001
+// 1 in binary: 0001
+// 0001 &
+// 0001
+// -----
+// 0001 (1 in decimal)
+console.log(1 & 1); // 1, bitwise AND
+// 2 in binary: 0010
+// 0010 &
+// 0001
+// -----
+// 0000
+console.log(2 & 1); // 0, bitwise AND
+// 3 in binary: 0011
+// 0011 &
+// 0001
+// -----
+// 0001
+console.log(3 & 1); // 1, bitwise AND
+// 4 in binary: 0100
+// 0100 &
+// 0001
+// -----
+// 0000
+console.log(4 & 1); // 0, bitwise AND
+
+// logical operators
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && false); // false
+console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || false); // false
+console.log(!true); // false
+console.log(!false); // true
+
+let number;
+
+// process number
+// number = false;
+
+console.log(number || 0);
+
+// Nullish coalescing operator (??)
+console.log(number ?? 0);
---
+++ classroom/ecma/statements.js
@@ -0,0 +1,120 @@
+const number = 10;
+
+// if statement
+// Truthy and falsy values
+// Falsy values: false, 0, -0, 0n, '', null, undefined, NaN
+// Truthy values: !Falsy values
+if (number > 0) console.log('Positive number');
+
+if (number > 0) {
+  console.log('Positive number');
+}
+
+// else statement
+if (number > 0) {
+  console.log('Positive number');
+} else {
+  console.log('Non-positive number');
+}
+
+// else if statement
+if (number > 0) {
+  console.log('Positive number');
+} else if (number < 0) {
+  console.log('Negative number');
+} else {
+  console.log('Zero');
+}
+
+// ternary operator (condition ? exprIfTrue : exprIfFalse)
+// number > 0 ? 'Positive number' : 'Non-positive number'
+// If truthy, return 'Positive number'
+// If falsy, return 'Non-positive number'
+const result = number > 0 ? 'Positive number' : 'Non-positive number';
+console.log(result);
+
+// switch statement
+const number1 = 1;
+const number2 = 2;
+const operator = '%'; // operator can be '+', '-', '*', '/'
+
+if (operator === '+') {
+  console.log(number1 + number2);
+} else if (operator === '-') {
+  console.log(number1 - number2);
+} else if (operator === '*') {
+  console.log(number1 * number2);
+} else if (operator === '/') {
+  console.log(number1 / number2);
+} else {
+  console.log('Invalid operator');
+}
+
+switch (operator) {
+  case '+':
+    console.log(number1 + number2);
+    break;
+  case '-':
+    console.log(number1 - number2);
+    break;
+  case '*':
+    console.log(number1 * number2);
+    break;
+  case '/':
+    console.log(number1 / number2);
+    break;
+  default:
+    console.log('Invalid operator');
+}
+
+switch (true) {
+  case number > 0:
+    console.log('Positive number');
+    break;
+  case number < 0:
+    console.log('Negative number');
+    break;
+  default:
+    console.log('Zero');
+}
+
+// while statement
+let count = 1;
+while (count <= 10) {
+  console.log(count);
+  count++; //count += 1; count = count + 1;
+}
+
+// do...while statement
+count = 1;
+do {
+  console.log(count);
+  count++;
+} while (count <= 10);
+
+// for statement
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+// TODO
+console.log(`
+00, 01, 02, 03, 04, 05, 06, 07, 08, 09,
+10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+60, 61, 62, 63, 64, 65, 66, 67, 68, 69,
+70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
+80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
+90, 91, 92, 93, 94, 95, 96, 97, 98, 99
+`);
+
+let numbers = '';
+
+for (let count = 1; count < 100; count++) {
+  numbers += count
+}
+
+console.log(numbers)
